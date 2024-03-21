5408c06d7de2153ca71f6781d5b1a47b77268c01
Darius Cosden <darius@cosdensolutions.io>
2024-03-21 20:44:15 +0100

    implemented teachable token for success

--- src/app/api/project-react/teachable/payment-success/route.ts
+++ src/app/api/project-react/teachable/payment-success/route.ts
@@ -4,7 +4,14 @@ import { NextResponse } from 'next/server';
 import { env } from '@/utils/env';
 
 // Updates a ConvertKit subscriber to mark that they have purchased the course
-export async function GET() {
+export async function GET(request: Request) {
+  const searchParams = new URL(request.url).searchParams;
+  const teachableToken = searchParams.get('teachable_token');
+
+  if (!teachableToken) {
+    return NextResponse.redirect(`${env.BASE_URL}`);
+  }
+
   const convertKitSubscriberId = cookies().get('convertKitSubscriberId');
   if (convertKitSubscriberId) {
     await fetch(
