8330e60b9277c9fa2eff0df68122c0877419076d
Darius Cosden <darius@cosdensolutions.io>
2024-04-04 17:34:29 +0200

    added console logs

--- src/utils/analytics/metaAnalytics.ts
+++ src/utils/analytics/metaAnalytics.ts
@@ -1,9 +1,11 @@
 import { MetaEvent } from './types';
 
 export function sendMetaAnalyticsEvent({ event, ...data }: MetaEvent) {
+  console.log('HERE', process.env.NODE_ENV);
   if (process.env.NODE_ENV === 'development') {
     return;
   }
+  console.log('AFTER', event, data);
 
   import('react-facebook-pixel')
     .then(module => module.default)
